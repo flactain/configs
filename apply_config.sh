3262688b6d4ae7bfda5a3f66e06fae8b8097ed6c
moriyamato <flactly@gmail.com>
2025-05-05 23:27:44 +0900

    feat: fix script for gather config files.

--- apply_config.sh
+++ apply_config.sh
@@ -8,17 +8,31 @@ EXCLUDE_DIRS=(".git")
 EXCLUDE_FILES=("README.md" "apply_config.sh")
 EXCLUDE_OPTIONS=""
 
+arg=${1:-}
+while [ -z "${arg}" ]; do
+  echo "Please type arguments of launch mode (apply/get)"
+  read arg
+done
+
+if [ "${arg}" = "apply" ]; then
+  echo "Apply config files of this project to your directory."
+elif [ "${arg}" = "get" ]; then
+  echo "Get config files from your directory."
+else
+  echo "Wrong arguments."
+  exit 1
+fi
+
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $script_dir
 
-function make_exclude() {
-  for dir in "${EXCLUDE_DIRS[@]}"; do
-    EXCLUDE_OPTIONS+="-path '*/$dir/*' -prune -o "
-  done
-  for pattern in "${EXCLUDE_FILES[@]}"; do
-    EXCLUDE_OPTIONS+="-name '$pattern' -prune -o "
-  done
-}
+for dir in "${EXCLUDE_DIRS[@]}"; do
+  EXCLUDE_OPTIONS+="-path '*/$dir/*' -prune -o "
+done
+for pattern in "${EXCLUDE_FILES[@]}"; do
+  EXCLUDE_OPTIONS+="-name '$pattern' -prune -o "
+done
+
 ###
 # main
 ###
@@ -30,5 +44,13 @@ while IFS= read -r -d $'\0' file; do
 done < <(eval "find . $EXCLUDE_OPTIONS -type f -print0")
 
 for i in ${!files[@]}; do
-  eval "cp -fb "${files[i]}" ~"${to_files[i]}""
+  if [ "${arg}" = "apply" ]; then
+    eval "cp -fb -S.backup "${files[i]}" ~"${to_files[i]}""
+  elif [ "${arg}" = "get" ]; then
+    eval "cp -f ~"${to_files[i]}" "${files[i]}""
+  else
+    echo "something go wrong."
+  fi
 done
+
+echo "apply files succesfully."
